b6f90e2c792ab55a2879ed874c4ebab98776b3b4
hjude-design <harrison@hjude.com>
2022-09-02 08:26:15 -0400

    Added function to concate strings to quote

--- Message.js
+++ Message.js
@@ -5,12 +5,24 @@ const Messages = {
     ],
     subjects:[
         'life',
-        'death'
+        'death',
+        'cats',
+        'humanity',
     ],
     statments:[
         'there is absurdity',
         'there is meaning',
-        'lies the root of all fear'
+        'lies the root of all fear',
+        'is the only perception of the universe'
     ]
 };
 
+const Quote = () =>{
+    const declaration = Messages.declarations[Math.floor(Math.random() * Messages.declarations.length)];
+    const subject = Messages.subjects[Math.floor(Math.random() * Messages.subjects.length)];
+    const statment = Messages.statments[Math.floor(Math.random() * Messages.statments.length)];
+    
+    return `${declaration} ${subject} ${statment}`;
+}
+
+console.log(Quote());
