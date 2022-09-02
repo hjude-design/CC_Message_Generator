fa23606981ff828364f1ed9f03bcced400d37d0f
hjude-design <harrison@hjude.com>
2022-09-02 08:04:51 -0400

    Added Quotes

--- Message.js
+++ Message.js
@@ -1,2 +1,6 @@
-console.log('works!');
-console.log('updated');
+const messages = [
+    'There is scarcely any passion without struggle.',
+    'We’re all going to die, all of us, what a circus! That alone should make us love each other but it doesn’t. We are terrorized and flattened by trivialities, we are eaten up by nothing.',
+    'All life is an experiment. The more experiments you make the better.',
+    'The rightful claim to dissent is an existential right of the individual.'
+];
