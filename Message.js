b45070ee3368f7c7e712a11ed66392c32e6a4ad5
hjude-design <harrison@hjude.com>
2022-09-02 08:13:20 -0400

    updated Messages arr to modular obj with arrs

--- Message.js
+++ Message.js
@@ -1,6 +1,16 @@
-const messages = [
-    'There is scarcely any passion without struggle.',
-    'We’re all going to die, all of us, what a circus! That alone should make us love each other but it doesn’t. We are terrorized and flattened by trivialities, we are eaten up by nothing.',
-    'All life is an experiment. The more experiments you make the better.',
-    'The rightful claim to dissent is an existential right of the individual.'
-];
+const Messages = {
+    declarations:[
+        'It is',
+        'Where there is'
+    ],
+    subjects:[
+        'life',
+        'death'
+    ],
+    statments:[
+        'there is absurdity',
+        'there is meaning',
+        'lies the root of all fear'
+    ]
+};
+
